d3f566a44b2d648a9539a350e8b9f5ca7004f078
phantware <phantmoney2011@gmail.com>
2020-04-28 21:49:55 +0100

    fortune search by id endpoint created

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const port = 8089;
 app.get("/fortunes", (req, res) => {
   res.status(200).send(fortunes);
 });
+
+app.get("/fortunes/random", (req, res) => {
+  res.status(200).send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get("/fortunes/:id", (req, res) => {
+  const items = req.params.id;
+  res.status(200).send(fortunes.find((f) => f.id == items));
+});
+
 app.listen(port, () => {
   console.log(`App listening at ${port}....`);
 });
+
+// const array = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// const findArray = array.find((items) => {
+//   return items.id === 2;
+// });
+// console.log(findArray);
